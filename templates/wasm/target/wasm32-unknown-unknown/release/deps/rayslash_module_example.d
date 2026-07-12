3c3e6b6c52bde4b9509a9b3702d8f46b2ced14bc
rayan6ms <rayan6ms@gmail.com>
2026-07-12 13:33:49 -0300

    Exclude template builds

--- templates/wasm/target/wasm32-unknown-unknown/release/deps/rayslash_module_example.d
+++
@@ -1,6 +0,0 @@
-/home/rayan/Documents/Projects/rslauncher-modules/rayslash-module-sdk/templates/wasm/target/wasm32-unknown-unknown/release/deps/rayslash_module_example.d: src/lib.rs src/bindings.rs
-
-/home/rayan/Documents/Projects/rslauncher-modules/rayslash-module-sdk/templates/wasm/target/wasm32-unknown-unknown/release/deps/rayslash_module_example.wasm: src/lib.rs src/bindings.rs
-
-src/lib.rs:
-src/bindings.rs:
